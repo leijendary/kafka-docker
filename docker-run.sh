7ca56d1ffe97cba0af1213e0142f87a5464cd029
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-02-16 23:43:21 +0800

    changed version to 1.0.0

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 docker volume create --name kafka_data
 docker network create kafka_network
-docker build --platform "linux/arm64" -t kafka:3.1.0 .
-docker run --name kafka -p 9092:9092 -p 29092:29092 -p 9093:9093 --mount source=kafka_data,target=/var/lib/kafka/data --network=kafka_network -d kafka:3.1.0
+docker build --platform "linux/arm64" -t kafka:1.0.0 .
+docker run --name kafka -p 9092:9092 -p 29092:29092 -p 9093:9093 --mount source=kafka_data,target=/var/lib/kafka/data --network=kafka_network -d kafka:1.0.0
