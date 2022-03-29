58d33c1459442e6a06724df694ab3429f9cf9c77
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-03-29 18:47:58 +0800

    enabled jaas

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
 FROM eclipse-temurin:17-jre
-RUN apt-get update && apt-get -y install wget && rm -rf /var/cache/apt/archives
+RUN apt-get update && apt-get -y install wget gettext-base && rm -rf /var/cache/apt/archives
 ENV SCALA_VERSION=2.13
 ENV KAFKA_VERSION=3.1.0
 ENV KAFKA_HOME=/opt/kafka
 ENV PATH=${PATH}:${KAFKA_HOME}/bin
 RUN mkdir $KAFKA_HOME
 RUN wget -qO- https://dlcdn.apache.org/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz | tar -C $KAFKA_HOME --strip 1 -xvz
+ENV KAFKA_BROKER_PASSWORD=broker-password
+ENV KAFKA_CLIENT_PASSWORD=client-password
 ENV KAFKA_LOG_DIRS=/var/lib/kafka/data
 ENV KAFKA_LOG_RETENTION_HOURS=168
 ENV KAFKA_HEAP_OPTS="-Xms512m -Xmx512m"
+COPY ./config/server.properties /opt/kafka/config/kraft/
+COPY ./config/kafka_broker_jaas.conf /opt/kafka/config/
 COPY ./entrypoint.sh /
 RUN ["chmod", "+x", "/entrypoint.sh"]
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-NODE_ID=${HOSTNAME:6}
-LISTENERS="INTERNAL://:9092,CONTROLLER://:9093,EXTERNAL://:9094"
-ADVERTISED_LISTENERS="INTERNAL://kafka-$NODE_ID.$SERVICE.$NAMESPACE.svc.cluster.local:9092,EXTERNAL://kafka-$NODE_ID.cluster.local:9094"
-LISTENER_SECURITY_PROTOCOL_MAP="INTERNAL:PLAINTEXT,CONTROLLER:PLAINTEXT,EXTERNAL:PLAINTEXT"
-INTER_BROKER_LISTENER_NAME="INTERNAL"
+export NODE_ID=${HOSTNAME:6}
+export CONTROLLER_QUORUM_VOTERS=""
 
-CONTROLLER_QUORUM_VOTERS=""
 for i in $(seq 0 $REPLICAS); do
     if [[ $i != $REPLICAS ]]; then
-        CONTROLLER_QUORUM_VOTERS="$CONTROLLER_QUORUM_VOTERS$i@kafka-$i.$SERVICE.$NAMESPACE.svc.cluster.local:9093,"
+        export CONTROLLER_QUORUM_VOTERS="$CONTROLLER_QUORUM_VOTERS$i@kafka-$i.$SERVICE.$NAMESPACE.svc.cluster.local:9093,"
     else
-        CONTROLLER_QUORUM_VOTERS=${CONTROLLER_QUORUM_VOTERS::-1}
+        export CONTROLLER_QUORUM_VOTERS=${CONTROLLER_QUORUM_VOTERS::-1}
     fi
 done
 
 mkdir -p $KAFKA_LOG_DIRS/$NODE_ID
 
-sed -i "
-    s|^node.id=.*|node.id=$NODE_ID|g
-    s|^controller.quorum.voters=.*|controller.quorum.voters=$CONTROLLER_QUORUM_VOTERS|g
-    s|^listeners=.*|listeners=$LISTENERS|g
-    s|^inter.broker.listener.name=.*|inter.broker.listener.name=$INTER_BROKER_LISTENER_NAME|g
-    s|^advertised.listeners=.*|advertised.listeners=$ADVERTISED_LISTENERS|g
-    s|^listener.security.protocol.map=.*|listener.security.protocol.map=$LISTENER_SECURITY_PROTOCOL_MAP|g
-    s|^log.dirs=.*|log.dirs=$KAFKA_LOG_DIRS/$NODE_ID|g
-    " /opt/kafka/config/kraft/server.properties
+if [[ -f "$KAFKA_LOG_DIRS/cluster_id" ]]; then
+    CLUSTER_ID=$(cat $KAFKA_LOG_DIRS/cluster_id)
+else
+    CLUSTER_ID=$(kafka-storage.sh random-uuid)
+    echo $CLUSTER_ID > $KAFKA_LOG_DIRS/cluster_id
+fi
 
-CLUSTER_ID=$(kafka-storage.sh random-uuid)
+envsubst < "/opt/kafka/config/kraft/server.properties" > "/opt/kafka/config/kraft/server.properties.updated"
+mv /opt/kafka/config/kraft/server.properties.updated /opt/kafka/config/kraft/server.properties
+
+envsubst < "/opt/kafka/config/kafka_broker_jaas.conf" > "/opt/kafka/config/kafka_broker_jaas.conf.updated"
+mv /opt/kafka/config/kafka_broker_jaas.conf.updated /opt/kafka/config/kafka_broker_jaas.conf
+
 kafka-storage.sh format -t $CLUSTER_ID -c /opt/kafka/config/kraft/server.properties
+
+export KAFKA_OPTS="-Djava.security.auth.login.config=/opt/kafka/config/kafka_broker_jaas.conf"
 
 exec kafka-server-start.sh /opt/kafka/config/kraft/server.properties
